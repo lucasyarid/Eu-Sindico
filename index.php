a5d4b3f82d3518ee555d2b476279ec5623a5595e
altendorfme <altendorfme@gmail.com>
2018-03-23 19:42:17 -0300

    add gallery and loading

--- index.php
+++ index.php
@@ -14,6 +14,47 @@
 	<meta name="description" content="" />
 </head>
 <body>
+	<div id="gallery">
+		<div class="swiper-container">
+			<div class="swiper-wrapper">
+				<div class="swiper-slide">
+					<img data-src="//picsum.photos/640/560" class="swiper-lazy">
+					<div class="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
+				</div>
+				<div class="swiper-slide">
+					<img data-src="//picsum.photos/320/560" class="swiper-lazy">
+					<div class="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
+				</div>
+				<div class="swiper-slide">
+					<img data-src="//picsum.photos/600/560" class="swiper-lazy">
+					<div class="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
+				</div>
+			</div>
+			<div class="swiper-pagination"></div>
+			<div class="swiper-button-next"></div>
+			<div class="swiper-button-prev"></div>
+		</div>
+	</div>
+	
+	<div id="loading" style="display: none;">
+		<div class="sending">
+			<div class="box">
+				<div class="spinner">
+					<div class="bounce1"></div>
+					<div class="bounce2"></div>
+					<div class="bounce3"></div>
+				</div>
+				<h5>Enviando...</h5>
+			</div>
+		</div>
+		<div class="sended">
+			<div class="box">
+				<i class="fa fa-check"></i>
+				<h5>Enviado!</h5>
+			</div>
+		</div>
+	</div>
+
 	<div class="swiper-container" id="app-container">	
 		<div class="swiper-wrapper">
 			<div class="swiper-slide menu" id="nav">
@@ -83,6 +124,22 @@
 			} else {
 				$('#menu-control').removeClass('open-menu-control');
 			}
+		});
+		
+		//
+		var gallery = new Swiper('#gallery .swiper-container', {
+			slidesPerView: 'auto',
+			centeredSlides: true,
+			spaceBetween: 30,
+			lazy: true,
+			pagination: {
+				el: '.swiper-pagination',
+				type: 'progressbar',
+			},
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
+			},
 		});
 		
 		// Notification Trigger
