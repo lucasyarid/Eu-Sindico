38787e284bf092b5b8665d9167cb7fd8d875ba72
altendorfme <altendorfme@gmail.com>
2018-04-10 23:24:31 -0300

    create timeline page

---
+++ timeline.php
@@ -0,0 +1,181 @@
+<nav id="notifications">
+	<h3>Notificações</h3>
+	<ul>
+		<li class="active">
+			<a href="#" class="d-flex">
+				<i class="align-self-center fa fa-exclamation-triangle"></i>
+				<span>
+					<strong>Nova proposta para analisar</strong>
+					<i>Prazo de 5 dias</i>
+				</span>
+				<time class="ml-auto">1 min</time>
+			</a>
+		</li>
+		<li class="active">
+			<a href="#" class="d-flex">
+				<i class="align-self-center fa fa-gavel"></i>
+				<span>
+					<strong>Nova proposta para analisar</strong>
+					<i>Prazo de 5 dias</i>
+				</span>
+				<time class="ml-auto">2d</time>
+			</a>
+		</li>
+		<li class="active">
+			<a href="#" class="d-flex">
+				<i class="align-self-center fa fa-check"></i>
+				<span>
+					<strong>Nova proposta para analisar</strong>
+					<i>Prazo de 5 dias</i>
+				</span>
+				<time class="ml-auto">2d</time>
+			</a>
+		</li>
+		<li class="disable">
+			<a href="#" class="d-flex">
+				<i class="align-self-center fa fa-times-circle-o"></i>
+				<span>
+					<strong>Nova proposta para analisar</strong>
+					<i>Prazo de 5 dias</i>
+				</span>
+				<time class="ml-auto">2d</time>
+			</a>
+		</li>
+		<li class="expired">
+			<a href="#" class="d-flex">
+				<i class="align-self-center fa fa-exclamation-triangle"></i>
+				<span>
+					<strong>Nova proposta para analisar</strong>
+					<i>Prazo de 5 dias</i>
+				</span>
+				<time class="ml-auto">1 sem</time>
+			</a>
+		</li>
+	</ul>
+</nav>
+<header id="content-header">
+	<div class="container d-flex justify-content-between align-items-center">
+		<div id="menu-control">
+			<span></span>
+		</div>
+		<h3>Aprovar Pedido</h3>
+		<div id="notification-trigger" class="notification">
+			<i class="fa fa-bell"></i><span class="badge badge-pill badge-danger">1</span>
+			<div class="notification-circle"></div>
+			<div class="notification-close"><i class="fa fa-close"></i></div>
+		</div>
+	</div>
+</header>
+<section id="timeline">
+	<header id="order-top" style="background-image: url(//picsum.photos/640/560)">
+		<div class="info">
+			<time>10/11/2017</time>
+			<h4>Guarda-sóis</h4>
+			<span class="order-reference align-text-top"><i class="fa fa-user-circle"></i> À pedido do Síndico</span>
+			<a href="#" class="gallery"><i class="fa fa-picture-o"></i>02 fotos</a>
+		</div>
+	</header>
+	<main class="container">
+		<div class="status d-flex justify-content-center mt-4 mb-4 text-center">
+			<div class="box mr-2 ml-2">
+				<div class="icon">
+					<i class="fa fa-inbox mt-3"></i>
+					<span class="badge badge-pill badge-success"><i class="fa fa-check"></i></span>
+				</div>
+				<p class="pt-2 mb-0">Pedido<br/>aprovado</p>
+			</div>
+			<div class="box mr-2 ml-2">
+				<div class="icon">
+					<i class="fa fa-newspaper-o mt-3"></i>
+					<span class="badge badge-pill badge-success"><i class="fa fa-check"></i></span>
+				</div>
+				<p class="pt-2 mb-0">Orçamento<br/>aprovado</p>
+			</div>
+			<div class="box mr-2 ml-2">
+				<div class="icon">
+					<i class="fa fa-shield mt-3"></i>
+					<span class="badge badge-pill badge-danger"><i class="fa fa-close"></i></span>
+				</div>
+				<p class="pt-2 mb-0">Realizado<br/>do prazo</p>
+			</div>
+		</div>
+		<h4>LINHA DO TEMPO COMPLETA</h4>
+		<ul class="mt-3">
+			<li class="row pb-3">
+				<time class="col-3 pr-0 pl-0 pt-3">10/11/2017</time>
+				<div class="col-2 pr-0 pl-0 text-center">
+					<div class="icon">
+						<i class="fa fa-info-circle mt-3"></i>
+						<span class="badge badge-pill badge-success"><i class="fa fa-check"></i></span>
+					</div>
+				</div>
+				<div class="box col-7 pt-2">
+					<h2 class="mt-0 mb-1">Pedido criado</h2>
+					<div class="content">
+						<h3 class="mt-0 mb-0"><i class="fa fa-chevron-down"></i> Visualizar</h3>
+					</div>
+				</div>
+			</li>
+			<li class="row pb-3">
+				<time class="col-3 pr-0 pl-0 pt-3">10/11/2017</time>
+				<div class="col-2 pr-0 pl-0 text-center">
+					<div class="icon">
+						<i class="fa fa-inbox mt-3"></i>
+						<span class="badge badge-pill badge-success"><i class="fa fa-check"></i></span>
+					</div>
+				</div>
+				<div class="box col-7 pt-2">
+					<h2 class="mt-0 mb-1">Pedido aprovado</h2>
+					<div class="content">
+						<h3 class="mt-0 mb-0"><i class="fa fa-chevron-down"></i> Visualizar</h3>
+					</div>
+				</div>
+			</li>
+			<li class="row pb-3">
+				<time class="col-3 pr-0 pl-0 pt-3">10/11/2017</time>
+				<div class="col-2 pr-0 pl-0 text-center">
+					<div class="icon">
+						<i class="fa fa-newspaper-o mt-3"></i>
+						<span class="badge badge-pill badge-success"><i class="fa fa-check"></i></span>
+					</div>
+				</div>
+				<div class="box col-7 pt-2">
+					<h2 class="mt-0 mb-1">Orçamento aprovado</h2>
+					<div class="content">
+						<h3 class="mt-0 mb-0"><i class="fa fa-chevron-down"></i> Visualizar</h3>
+					</div>
+				</div>
+			</li>
+			<li class="row pb-3">
+				<time class="col-3 pr-0 pl-0 pt-3">10/11/2017</time>
+				<div class="col-2 pr-0 pl-0 text-center">
+					<div class="icon">
+						<i class="fa fa-cog mt-3"></i>
+						<span class="badge badge-pill badge-success"><i class="fa fa-check"></i></span>
+					</div>
+				</div>
+				<div class="box col-7 pt-2">
+					<h2 class="mt-0 mb-1">Inicio do serviço</h2>
+					<div class="content">
+						<h3 class="mt-0 mb-0"><i class="fa fa-chevron-down"></i> Visualizar</h3>
+					</div>
+				</div>
+			</li>
+			<li class="row pb-3">
+				<time class="col-3 pr-0 pl-0 pt-3">10/11/2017</time>
+				<div class="col-2 pr-0 pl-0 text-center">
+					<div class="icon">
+						<i class="fa fa-shield mt-3"></i>
+						<span class="badge badge-pill badge-danger"><i class="fa fa-close"></i></span>
+					</div>
+				</div>
+				<div class="box col-7 pt-2">
+					<h2 class="mt-0 mb-1">Pedido criado</h2>
+					<div class="content">
+						<h3 class="mt-0 mb-0"><i class="fa fa-chevron-down"></i> Visualizar</h3>
+					</div>
+				</div>
+			</li>
+		</ul>
+	</main>
+</section>
