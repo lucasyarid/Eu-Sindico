799584395aa8de3f7473cc4b0c7bb7488a700914
Lucas Yarid <lucas.yarid@gmail.com>
2018-09-03 17:08:45 -0300

    create scss architecture

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,94 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <nav id="notifications">
+      <h3>Notificações</h3>
+      <ul>
+        <li class="active">
+          <a href="#" class="d-flex">
+            <i class="align-self-center fa fa-exclamation-triangle"></i>
+            <span>
+              <strong>Nova proposta para analisar</strong>
+              <i>Prazo de 5 dias</i>
+            </span>
+            <time class="ml-auto">1 min</time>
+          </a>
+        </li>
+        <li class="active">
+          <a href="#" class="d-flex">
+            <i class="align-self-center fa fa-gavel"></i>
+            <span>
+              <strong>Nova proposta para analisar</strong>
+              <i>Prazo de 5 dias</i>
+            </span>
+            <time class="ml-auto">2d</time>
+          </a>
+        </li>
+        <li class="active">
+          <a href="#" class="d-flex">
+            <i class="align-self-center fa fa-check"></i>
+            <span>
+              <strong>Nova proposta para analisar</strong>
+              <i>Prazo de 5 dias</i>
+            </span>
+            <time class="ml-auto">2d</time>
+          </a>
+        </li>
+        <li class="disable">
+          <a href="#" class="d-flex">
+            <i class="align-self-center fa fa-times-circle-o"></i>
+            <span>
+              <strong>Nova proposta para analisar</strong>
+              <i>Prazo de 5 dias</i>
+            </span>
+            <time class="ml-auto">2d</time>
+          </a>
+        </li>
+        <li class="expired">
+          <a href="#" class="d-flex">
+            <i class="align-self-center fa fa-exclamation-triangle"></i>
+            <span>
+              <strong>Nova proposta para analisar</strong>
+              <i>Prazo de 5 dias</i>
+            </span>
+            <time class="ml-auto">1 sem</time>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <header id="content-header">
+      <div class="container d-flex justify-content-between align-items-center">
+        <div id="menu-control">
+          <span></span>
+        </div>
+        <h2>Eu Síndico</h2>
+        <div id="notification-trigger" class="notification">
+          <i class="fa fa-bell"></i><span class="badge badge-pill badge-danger">1</span>
+          <div class="notification-circle"></div>
+          <div class="notification-close"><i class="fa fa-close"></i></div>
+        </div>
+      </div>
+    </header>
+    <section id="home">
+      <header style="background-image: url(//picsum.photos/640/560)"></header>
+      <main>
+        <div class="container">
+          <h3>
+            <span>Condomínio</span>
+            Quatro Estações Alto da Lapa
+          </h3>
+          <nav>
+            <div class="row">
+              <div class="option col col-4"><a href="#"><span class="bubble orders"><i class="fa fa-folder"></i><i class="badge badge-pill">3</i></span>Pedidos</a></div>
+              <div class="option col col-4"><a href="#"><span class="bubble finished"><i class="fa fa-check"></i><i class="badge badge-pill">3</i></span>Concluidos</a></div>
+              <div class="option col col-4"><a href="#"><span class="bubble budgeting"><i class="fa fa-wpforms"></i><i class="badge badge-pill">3</i></span>Orçamentos</a></div>
+              <div class="option col col-4"><a href="#"><span class="bubble configuration"><i class="fa fa-cog"></i></span>Ajustes</a></div>
+              <div class="option col col-4"><a href="#"><span class="bubble in-progress"><i class="fa fa-play"></i><i class="badge badge-pill">3</i></span>Em Progresso</a></div>
+              <div class="option col col-4"><a href="#"><span class="bubble new-order"><i class="fa fa-plus"></i></span>Novo Pedido</a></div>
+            </div>
+          </nav>
+        </div>
+      </main>
+    </section>
   </div>
 </template>
 
@@ -38,21 +100,3 @@ export default {
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
