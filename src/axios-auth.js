8c49c2b934ad7ee313ddbfe84e9df6c26401d765
Lucas Yarid <lucas.yarid@gmail.com>
2018-11-19 00:12:49 -0200

    add interceptor to axios apitoken

--- src/axios-auth.js
+++ src/axios-auth.js
@@ -1,10 +1,28 @@
 import axios from 'axios'
 
+// var apiToken = localStorage.getItem('apiToken')
+
 const instance = axios.create({
   baseURL: 'https://api.eusindico.com.br',
   headers: {
-    'api_token': `${localStorage.getItem('apiToken')}`
+    'api_token': localStorage.getItem('apiToken')
   }
 })
+
+instance.interceptors.request.use(
+  (config) => {
+    let apiToken = localStorage.getItem('apiToken')
+
+    if (apiToken) {
+      config.headers['api_token'] = apiToken
+    }
+
+    return config
+  },
+
+  (error) => {
+    return Promise.reject(error)
+  }
+)
 
 export default instance
